3cfca85dae37f9f7d0e5d8a83291158cb2624974
Sebburk <110697287+Sebburk@users.noreply.github.com>
2023-05-16 09:45:25 -0700

    Update code.py

--- code.py
+++ code.py
@@ -3,5 +3,5 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("A939RX0Q048SBEA.csv")
 print(df.columns)
-df.plot(x= "Date", y = ['A939RX0Q048SBEA'])
+df.plot(x= "DATE", y = ['A939RX0Q048SBEA'])
 plt.show() 
